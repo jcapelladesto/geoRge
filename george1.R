3afcece39586dc1dab5eba05a205e9517ff094bb
jcapelladesto <j.capellades.to@gmail.com>
2015-07-27 11:26:38 +0200

    Update george1.R

--- george1.R
+++ george1.R
@@ -1,5 +1,5 @@
-PuInc_seeker <- function (XCMSet=NULL, ULtag=NULL, Ltag=NULL, separator="_", sep.pos=NULL, fc.threshold=1.2, p.value.threshold=0.05,
-PuInc.int.lim=5000, ...){
+PuInc_seeker <- function (XCMSet=NULL, ULtag=NULL, Ltag=NULL, separator="_", sep.pos=NULL, fc.threshold=1.2, 
+p.value.threshold=0.05, PuInc.int.lim=5000, ...){
 
 X1 <- groupval(XCMSet, value = "maxo") # geoRge has only been used on "maxo". I suppose it works for others too.
 D1 <- data.frame(t(X1))
@@ -104,13 +104,15 @@ list(
 )
 }
 
-basepeak_finder <- function(PuIncR=NULL, XCMSet=NULL, ULtag=NULL, Ltag=NULL, separator="_", sep.pos=NULL, fc.threshold=1.2, p.value.threshold=0.05,
-PuInc.int.lim=5000, UL.atomM=NULL, L.atomM=NULL, ppm.s=NULL, rt.win.min=1, Basepeak.minInt=NULL, BP.Percentage=0.7, noise.quant=0.2,  ...) {
+basepeak_finder <- function(PuIncR=NULL, XCMSet=NULL, ULtag=NULL, Ltag=NULL, separator="_", sep.pos=NULL,
+UL.atomM=NULL, L.atomM=NULL, ppm.s=NULL, rt.win.min=1, Basepeak.minInt=NULL, BP.Percentage=0.7, noise.quant=0.2,  ...) {
 
 X1 <- groupval(XCMSet, value = "maxo") # geoRge has only been used on "maxo". I suppose it works for others too.
 D1 <- data.frame(t(X1))
 colnames(D1) <- as.character(1:nrow(X1))
 filtsamps <- 1:ncol(D1)
+
+classv <- as.factor(XCMSet@phenoData$class) # sample classes (use separate folders per group when running XCMS)
 
 res_inc <- PuIncR[["PuInc"]]
 compinc <- PuIncR[["PuInc_conditions"]]
@@ -295,4 +297,149 @@ georgedf <- do.call("rbind",george) # This should be a data.frame with 6 + Numbe
 ##' Column 6 (inc_condition): Sample condition where the incorporation was detected.
 ##' 						If it was detected in more than one condition, they are separated by ";".
 return(georgedf)
+}
+
+
+#### Calculate the percentage of incorporation and compare it versus a "control" condition for each "inc_id" ####
+label_compare <- function(geoRgeR=NULL, PuIncR=NULL, XCMSet=NULL, ULtag=NULL, Ltag=NULL, separator="_", sep.pos=NULL,
+UL.atomM=NULL, L.atomM=NULL, ppm.s=NULL, rt.win.min=1, control.cond=NULL, fc.vs.Control=1, p.value.vs.Control=0.05, Show.bp= T) {
+
+georgedf <- geoRgeR
+
+X1 <- groupval(XCMSet, value = "maxo") # geoRge has only been used on "maxo". I suppose it works for others too.
+D1 <- data.frame(t(X1))
+colnames(D1) <- as.character(1:nrow(X1))
+filtsamps <- 1:ncol(D1)
+classv <- as.factor(XCMSet@phenoData$class) # sample classes (use separate folders per group when running XCMS)
+xgroup <- cbind(XCMSet@groups[filtsamps,c("mzmed", "rtmed")],t(D1)) 
+
+conditions <- sapply(levels(classv), USE.NAMES=F, function(x) {
+	if (sep.pos=="f") {
+	a <- strsplit(x, split = paste(ULtag, separator, sep=""))[[1]][2]
+	} else {
+	a <- strsplit(x, split  =paste(separator, ULtag, sep=""))[[1]][1]
+	}
+	return (a)
+})
+conditions <- unique(conditions[-which(is.na(conditions))])
+
+percent.incorp <- lapply(unique(georgedf$inc_id),function(y){
+	
+	inc_id_features <- georgedf[which(georgedf$inc_id==y),] 
+	inc_id_int <- inc_id_features[,7:ncol(inc_id_features)]
+	
+	rts <- inc_id_features$rtmed  
+	rt_range <- c(min(rts),max(rts))
+	
+	inc_isot <- max(inc_id_features$atoms)+1
+	isot_m <- inc_id_features[1,"mzmed"]+(inc_isot*mass_diff) # mass of isotope of the incorporation
+	
+	isot_id <- lapply(isot_m,function(x){ # seek isotope of the incorporation in raw data
+		mass_range <- c(x-ppm.s*(x/1e6),x+ppm.s*(x/1e6))
+		a <- which(xgroup[,"mzmed"]>=mass_range[1] & xgroup[,"mzmed"]<=mass_range[2])  
+		b <- which(xgroup[,"rtmed"]>=rt_range[1] & xgroup[,"rtmed"]<=rt_range[2])
+		r <- intersect(a,b)
+		return(r)
+	})
+	
+	isot_id <- unlist(isot_id)
+	
+	if(length(isot_id)<1){
+		all_id <- xgroup[as.character(inc_id_features$feature_id),] 
+	}else{
+		isot_id <- isot_id[1]
+		all_id <- c(as.character(inc_id_features$feature_id),isot_id)
+		all_id <- xgroup[all_id,]  
+	}
+	
+	all_id_int <- all_id[,3:ncol(all_id)] # intensities
+	
+	inc_percent <- sapply(conditions,function(x){
+		inc_id_intL <- inc_id_int[,grep(paste(Ltag,x,sep=separator),colnames(inc_id_int))]
+		all_id_intL <- all_id_int[,grep(paste(Ltag,x,sep=separator),colnames(all_id_int))]
+		
+		inc_cal <- sapply(1:ncol(inc_id_intL),function(x) {(inc_id_intL[,x]/sum(all_id_intL[,x]))*100})
+		return(inc_cal)
+	})
+	colnames(inc_percent) <- conditions
+	
+	atoms <- inc_id_features$atoms
+	rownames(inc_percent) <- rep(atoms,length.out=nrow(inc_percent)) 
+	
+	mean_inc <- sapply(conditions,USE.NAMES=T,simplify=T,function(x){
+		sapply(atoms,function (z){
+			inc_p_v <- inc_percent[which(rownames(inc_percent)==z),x]
+			inc_p_m <- mean(inc_p_v)  
+			return(inc_p_m)
+		})
+	})
+	colnames(mean_inc) <- paste0(colnames(mean_inc),"_MEAN")
+	
+	sd_inc <- sapply(conditions,USE.NAMES=T,simplify=T,function(x){
+		sapply(atoms,function (z){
+			inc_p_v <- inc_percent[which(rownames(inc_percent)==z),x]
+			inc_p_s <- sd(inc_p_v)  
+			return(inc_p_s)
+		})
+	})
+	colnames(sd_inc) <- paste0(colnames(sd_inc),"_SD")
+	
+	noncontrol <- setdiff(conditions,control)
+	
+	pvals <- sapply(noncontrol,function (x){
+		sapply(atoms,function(y){
+			a <- try(t.test(inc_percent[which(rownames(inc_percent)==y),which(conditions==x)],
+					inc_percent[which(rownames(inc_percent)==y),which(conditions==control)],
+					var.equal=T)$p.value,silent=T)
+			if(is(a,"try-error")){a <- 1}
+			return(a)
+		})
+	})
+	
+	fct <- sapply(noncontrol,simplify=T,function (x){
+		sapply(1:nrow(mean_inc),function(y){ 
+			case <- mean_inc[y,which(conditions==x)]
+			control <- mean_inc[y,which(conditions==control)]
+			FC <- case/control;
+			FC2 <- (-(control/case))
+			FC[FC<1] <- FC2[FC<1]
+			names(FC) <- NULL
+			return(FC)
+		})
+	})
+	
+	comp <- sapply(1:nrow(pvals),function(x) {
+		t <- which(pvals [x, ] < .value.vs.Control)
+		if(length(t) == 0) {
+		t <- ""
+		return(t)
+	}else{
+		up <- which(fct[x,names(t)]>fc.vs.Control)
+		down <- which(fct[x,names(t)]<(-fc.vs.Control))
+		if(length(up)!=0){names(t)[up] <- paste("UP",names(t)[up],sep="_")}
+		if(length(down)!=0){names(t)[down] <- paste("DOWN",names(t)[down],sep="_")}
+		return(names(t))
+		}
+	})
+	
+	if(is.matrix(comp)){
+		comp <- sapply(1:ncol(comp),function(x){
+			a <- paste(comp[,x],collapse=";")
+		})
+	} else {
+		comp <- lapply(1:length(comp),function(x) paste(comp[[x]], collapse=";"))
+		comp <- unlist(comp)
+	}
+	
+	comp[1] <- "Base peak"
+	
+	r <- data.frame("Comparison" = comp, mean_inc, sd_inc)
+	if (!Show.bp) { r[1, ] <- rep("Base Peak", times=ncol(r))}
+	return(r)
+})
+
+percent.incorpdf <- do.call("rbind",percent.incorp)
+
+return(percent.incorpdf)
+
 }
